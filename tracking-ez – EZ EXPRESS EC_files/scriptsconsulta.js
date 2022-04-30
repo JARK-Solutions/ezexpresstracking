a7d51ebf945d10c8a67d0eca98e3e0bd0459c4a7
admin-obsidian <64256795+admin-obsidian@users.noreply.github.com>
2022-04-29 23:42:13 -0500

    first

---
+++ tracking-ez – EZ EXPRESS EC_files/scriptsconsulta.js
@@ -0,0 +1,30 @@
+function Startpo() {
+  kalendarik = window.open("../po/trackPickup_Order.php", "kalendarik" , "location=0, menubar=0, scrollbars=1, status=0, titlebar=0, toolbar=0, directories=0, resizable=0,copyhistory=0, width=400, height=400, top=50, left=250");
+
+  kalendarik.resizeTo(700,700);
+  kalendarik.moveTo(250, 50);
+}
+function Startwr() {
+  kalendarik = window.open("../wr/trackWarehouse_Receipt.php", "kalendarik" , "location=0, menubar=0, scrollbars=1, status=0, titlebar=0, toolbar=0, directories=0, resizable=0,copyhistory=0, width=400, height=400, top=50, left=250");
+
+  kalendarik.resizeTo(700,700);
+  kalendarik.moveTo(250, 50);
+}
+function Startwb() {
+  kalendarik = window.open("../wr/trackWay_Bill.php", "kalendarik" , "location=0, menubar=0, scrollbars=1, status=0, titlebar=0, toolbar=0, directories=0, resizable=0,copyhistory=0, width=400, height=400, top=50, left=250");
+
+  kalendarik.resizeTo(700,700);
+  kalendarik.moveTo(250, 50);
+}
+function Startguialabel() {
+  kalendarik = window.open("../wr/trackguialabel.php", "kalendarik" , "location=0, menubar=0, scrollbars=1, status=0, titlebar=0, toolbar=0, directories=0, resizable=0,copyhistory=0, width=600, height=600, top=50, left=250");
+
+  kalendarik.resizeTo(900,700);
+  kalendarik.moveTo(250, 50);
+}
+function Start77() {
+  kalendarik = window.open("$laruta", "kalendarik" , "location=0, menubar=0, scrollbars=1, status=0, titlebar=0, toolbar=0, directories=0, resizable=0,copyhistory=0, width=400, height=400, top=50, left=250");
+
+  kalendarik.resizeTo(700,700);
+  kalendarik.moveTo(250, 50);
+}
